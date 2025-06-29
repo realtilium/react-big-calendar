ca59229f104f15348513a9e0a73632971d8e2176
jchonde <jc.honde@gmail.com>
2025-06-29 23:49:55 +0200

    fix(views): perf

--- src/Views.js
+++ src/Views.js
@@ -1,16 +1,37 @@
-import { views } from './utils/constants'
+import React from 'react'
 import Month from './Month'
-import Day from './Day'
 import Week from './Week'
-import WorkWeek from './WorkWeek'
+import Day from './Day'
 import Agenda from './Agenda'
+import WorkWeek from './WorkWeek'
+import { dequal } from 'dequal'
 
-const VIEWS = {
-  [views.MONTH]: Month,
-  [views.WEEK]: Week,
-  [views.WORK_WEEK]: WorkWeek,
-  [views.DAY]: Day,
-  [views.AGENDA]: Agenda,
+function withStatics(Wrapped, Source) {
+  ;['title', 'navigate', 'range'].forEach((k) => {
+    if (Source[k]) Wrapped[k] = Source[k]
+  })
+  return Wrapped
 }
 
-export default VIEWS
+function normalizeDay(date) {
+  if (!(date instanceof Date)) return date
+  const d = new Date(date)
+  d.setHours(0, 0, 0, 0)
+  return d.getTime()
+}
+
+const areEqual = (prev, next) => {
+  return (
+    dequal(prev.events, next.events) &&
+    dequal(prev.backgroundEvents, next.backgroundEvents) &&
+    normalizeDay(prev.date) === normalizeDay(next.date)
+  )
+}
+
+export default {
+  month: withStatics(React.memo(Month, areEqual), Month),
+  week: withStatics(React.memo(Week, areEqual), Week),
+  day: withStatics(React.memo(Day, areEqual), Day),
+  agenda: withStatics(React.memo(Agenda, areEqual), Agenda),
+  work_week: withStatics(React.memo(WorkWeek, areEqual), WorkWeek),
+}
